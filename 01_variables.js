e9140ecbf2d341b66dd97e58719f64bb63422709
Ankit Thakur <at667448@gmail.com>
2025-12-06 19:36:51 +0530

    Update folder

--- 01_variables.js
+++
@@ -1,9 +0,0 @@
-// Variable are use to define store a value in different storage area with different names.
-// Variable can define with the help of var, const & let 
-
-const constName = "ankit";
-lastName = "Thakur";
-var  nameItem = "Classified";
-let nameId = 75500;
-nameId = "7000"
-console.table([constName, lastName, nameItem, nameId]);
---
+++ Basic/03comperision.js
@@ -0,0 +1,21 @@
+let a = "1";
+let b = 10;
+// Normal Comperision operator 
+
+console.log( a < b);
+console.log( a > b);
+console.log( a <= b);
+console.log( a >= b);
+console.log( a == b);
+
+
+
+console.log( null > 0);
+console.log( null < 0);
+console.log( null >= 0);
+console.log( undefined > 0);
+console.log( undefined < 0);
+console.log( undefined >= 0);
+
+
+console.log( a === b ); // strict comperison operator
