666b61a4d9dc7e0ce25c5e574ff3838f9a0ba21e
AnkitCodesdesk <ankit@codesdesk.com>
2025-12-09 19:28:44 +0530

    Arrays

--- Non Primitive variables/01_arrays.js
+++ Non Primitive variables/01_arrays.js
@@ -2,27 +2,17 @@
 
 
 var myArray = [ 0, 1, 2, 3, 4, 5];
-<<<<<<< Updated upstream
-var myArray = [ 0, 1, 2, 3, 4, 5];
-=======
 var myArr = [ 0, 1, 2, 3, 4, 5];
 var myArray1 = myArray;
->>>>>>> Stashed changes
 var test = ["test", "test2", "test3"];
 console.log('test');
 // for( i=0; i < myArray.length; i++){
 //     console.log(myArray[i]);
 // }
 
-<<<<<<< Updated upstream
-myArray.pop();
-console.log(`Remove last value of string using pop() ${myArray}`);
-myArray.push(6);
-console.log(`Remove last value of string using push() ${myArray}`);
 
 myArray.shift();
 console.log(myArray);
-=======
 myArray1.pop();
 console.log(`Remove last value of string using pop() ${myArray1}`);
 myArray1.push(6);
@@ -54,4 +44,3 @@ console.log(`In this operation we can use Array slice(1, 3) function it can slic
 var myArrCopy1 = newArr.splice(1, 3);
 console.log(`In this operation we can use Array splice(1, 3) function it can remove premanent value from index 1 to last index 3.it can remove ${myArrCopy1}` );
 console.log(`After splice our variable is ${newArr}`);
->>>>>>> Stashed changes
